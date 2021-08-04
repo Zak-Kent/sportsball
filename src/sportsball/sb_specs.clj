cfd2898e55de7deddbce82bf379da20818a0b265
Zak Kent <zakkent@Zaks-MacBook-Pro.local>
2021-08-03 21:51:49 -0700

    Restrict spec to only allow book-odds where both values either ints or nils

--- src/sportsball/sb_specs.clj
+++ src/sportsball/sb_specs.clj
@@ -4,10 +4,14 @@
   (:import java.util.Date))
 
 ;; Books
-(s/def ::odds int?)
+(s/def ::odds (s/or :int int? :nil nil?)) ; s/nilable doesn't generate nils
 (s/def ::home-odds ::odds)
 (s/def ::away-odds ::odds)
-(s/def ::book-odds (s/keys :req [::home-odds ::away-odds]))
+(s/def ::book-odds (s/and
+                    (s/keys :req [::home-odds ::away-odds])
+                    (fn [{:keys [::home-odds ::away-odds]}]
+                      ;; due to the s/or in ::odds home/away-odds are MapEntrys
+                      (= (type (val home-odds)) (type (val away-odds))))))
 
 (s/def ::bovada ::book-odds)
 (s/def ::betonline ::book-odds)
@@ -15,8 +19,6 @@
 (s/def ::heritage ::book-odds)
 (s/def ::intertops ::book-odds)
 (s/def ::youwager ::book-odds)
-(s/def ::book-id #{::bovada ::betonline ::bookmaker
-                   ::heritage ::intertops ::youwager})
 
 (def team-abrv #{"ARI" "ATL" "BAL" "BOS" "CHC" "CWS" "CIN" "CLE" "COL"
                  "DET" "FLA" "HOU" "KAN" "LAA" "LAD" "MIL" "MIN" "NYM"
@@ -30,12 +32,16 @@
 
 (s/def ::timestamp inst?)
 
-(s/def ::odds-info (s/keys :req [::teams ::timestamp]
-                           :opt [::book-id]))
+(s/def ::odds-info (s/keys :req [::teams ::timestamp ::bovada ::betonline
+                                 ::bookmaker ::heritage ::intertops ::youwager]))
 
 (s/conform ::odds-info {::timestamp (Date.)
                         ::teams {::home "ARI" ::away "TEX"}
                         ::bovada {::home-odds -155 ::away-odds 34}
-                        ::heritage {::home-odds 55 ::away-odds 34}})
+                        ::heritage {::home-odds 55 ::away-odds 34}
+                        ::betonline {::home-odds 55 ::away-odds nil}
+                        ::bookmaker {::home-odds 55 ::away-odds 34}
+                        ::intertops {::home-odds 55 ::away-odds 34}
+                        ::youwager {::home-odds 55 ::away-odds 34}})
 
 (gen/generate (s/gen ::odds-info))
