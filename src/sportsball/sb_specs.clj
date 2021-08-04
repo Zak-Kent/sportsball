d628075421d65f7187fb20800e5a5c38c7a2a8fb
Zak Kent <zakkent@Zaks-MacBook-Pro.local>
2021-08-03 20:59:14 -0700

    Switch to fully qualified names for specs

--- src/sportsball/sb_specs.clj
+++ src/sportsball/sb_specs.clj
@@ -4,38 +4,38 @@
   (:import java.util.Date))
 
 ;; Books
-(s/def :sb/odds int?)
-(s/def :sb/home-odds :sb/odds)
-(s/def :sb/away-odds :sb/odds)
-(s/def :sb/book-odds (s/keys :req [:sb/home-odds :sb/away-odds]))
+(s/def ::odds int?)
+(s/def ::home-odds ::odds)
+(s/def ::away-odds ::odds)
+(s/def ::book-odds (s/keys :req [::home-odds ::away-odds]))
 
-(s/def :sb/bovada :sb/book-odds)
-(s/def :sb/betonline :sb/book-odds)
-(s/def :sb/bookmaker :sb/book-odds)
-(s/def :sb/heritage :sb/book-odds)
-(s/def :sb/intertops :sb/book-odds)
-(s/def :sb/youwager :sb/book-odds)
-(s/def :sb/book-id #{:sb/bovada :sb/betonline :sb/bookmaker
-                     :sb/heritage :sb/intertops :sb/youwager})
+(s/def ::bovada ::book-odds)
+(s/def ::betonline ::book-odds)
+(s/def ::bookmaker ::book-odds)
+(s/def ::heritage ::book-odds)
+(s/def ::intertops ::book-odds)
+(s/def ::youwager ::book-odds)
+(s/def ::book-id #{::bovada ::betonline ::bookmaker
+                   ::heritage ::intertops ::youwager})
 
 (def team-abrv #{"ARI" "ATL" "BAL" "BOS" "CHC" "CWS" "CIN" "CLE" "COL"
                  "DET" "FLA" "HOU" "KAN" "LAA" "LAD" "MIL" "MIN" "NYM"
                  "NYY" "OAK" "PHI" "PIT" "SD" "SF" "SEA" "STL" "TB"
                  "TEX" "TOR" "WAS"})
 
-(s/def :sb/team-id team-abrv)
-(s/def :sb/home :sb/team-id)
-(s/def :sb/away :sb/team-id)
-(s/def :sb/teams (s/keys :req [:sb/home :sb/away]))
+(s/def ::team-id team-abrv)
+(s/def ::home ::team-id)
+(s/def ::away ::team-id)
+(s/def ::teams (s/keys :req [::home ::away]))
 
-(s/def :sb/timestamp inst?)
+(s/def ::timestamp inst?)
 
-(s/def :sb/odds-info (s/keys :req [:sb/teams :sb/timestamp]
-                             :opt [:sb/book-id]))
+(s/def ::odds-info (s/keys :req [::teams ::timestamp]
+                           :opt [::book-id]))
 
-(s/conform :sb/odds-info {:sb/timestamp (Date.)
-                          :sb/teams {:sb/home "ARI" :sb/away "TEX"}
-                          :sb/bovada {:sb/home-odds -155 :sb/away-odds 34}
-                          :sb/heritage {:sb/home-odds 55 :sb/away-odds 34}})
+(s/conform ::odds-info {::timestamp (Date.)
+                        ::teams {::home "ARI" ::away "TEX"}
+                        ::bovada {::home-odds -155 ::away-odds 34}
+                        ::heritage {::home-odds 55 ::away-odds 34}})
 
-(gen/generate (s/gen :sb/odds-info))
+(gen/generate (s/gen ::odds-info))
