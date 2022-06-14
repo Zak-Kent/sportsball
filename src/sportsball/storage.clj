056aa9ee24275cd3ad69a16034b08d681a204759
Zak-Kent <zak.kent@gmail.com>
2022-06-14 16:37:07 -0700

    Store game matchup with check for existing matchup
    This commit extracts a matchup map Ex. {:time ... :teams "tA-tB"} from
    an incoming odds bundle. Performs a check for an existing matchup of the
    same teams on a given date and returns the matchup_id for the game. This
    id is used as a FK in the odds table.
    
    Two issues with this approach are not being able to handle double headers
    or games which cross the midinight boundry in local time. In the former case
    you'll end up with two seperate games having the same matchup_id and in the
    latter case you'll have the same game split across two matchup_ids. These
    issues may be addressed in a later commit, or I may leave it swept under the
    rug ;)

--- src/sportsball/storage.clj
+++ src/sportsball/storage.clj
@@ -1,5 +1,7 @@
 (ns sportsball.storage
   (:require [next.jdbc :as jdbc]
+            [next.jdbc.sql :as sql]
+            [java-time :as t]
             [clojure.string :as str]))
 
 (def db {:dbtype "postgresql"
@@ -14,9 +16,9 @@
 
 (def matchup-table-sql
   ["create table if not exists matchup (
-      matchup_id INTEGER PRIMARY KEY,
+      matchup_id SERIAL PRIMARY KEY,
       time TIMESTAMPTZ NOT NULL,
-      matchup VARCHAR NOT NULL)"])
+      teams VARCHAR NOT NULL)"])
 
 (def odds-table-sql
   ["create table if not exists odds (
@@ -24,7 +26,7 @@
       lines JSONB NOT NULL,
       home_score SMALLINT NOT NULL,
       away_score SMALLINT NOT NULL,
-      matchup_id INTEGER,
+      matchup_id INTEGER NOT NULL,
       CONSTRAINT fk_matchup
         FOREIGN KEY (matchup_id)
           REFERENCES matchup(matchup_id))"])
@@ -37,3 +39,39 @@
 
 (defn check-odds []
   (jdbc/execute! db [" SELECT * from odds; "]))
+
+(defn odds->matchup [odds]
+  (let [ts (:timestamp odds)
+        teams (->> (:teams odds)
+                     vals
+                     (str/join "-"))]
+    ;; the rows returned by jdbc are namespaced maps
+    #:matchup{:time ts :teams teams}))
+
+(defn get-local-date [d]
+  (t/format :iso-local-date
+            (t/local-date-time d (t/zone-id))))
+
+(defn check-matchup [matchup]
+  "Takes an incoming matchup and returns a seq of any existing
+   matchups for the same date already in the DB"
+  (let [get-date (fn [m] (-> (:matchup/time m)
+                         get-local-date))
+        db-matchups (sql/query
+                     db
+                     ["select * from matchup where teams = ?"
+                      (:matchup/teams matchup)])
+        in-matchup (get-date matchup)]
+    (filter (fn [m] (= (get-date m) in-matchup)) db-matchups)))
+
+(defn store-matchup [odds-info]
+  "Takes an incoming odds bundle decides whether or not to store
+   a new matchup. Returns the matchup_id used as a FK in the odds
+   table."
+  (let [mup (odds->matchup odds-info)
+        existing-mup (check-matchup mup)]
+    (if (seq existing-mup)
+      ;; TODO handle case if there are matching games on the same date
+      ;; double headers, etc
+      (:matchup/matchup_id (first existing-mup))
+      (:matchup/matchup_id (sql/insert! db :matchup mup)))))
--- test/sportsball/core_test.clj
+++ test/sportsball/core_test.clj
@@ -1,16 +1,33 @@
 (ns sportsball.core-test
   (:require [clojure.test :refer :all]
+            [java-time :as t]
             [sportsball.testutils :as tu]
             [next.jdbc.sql :as sql]
-            [sportsball.core :refer :all]))
+            [malli.generator :as mg]
+            [sportsball.sb-specs :as sbspec]
+            [sportsball.storage :as store]))
 
 (defn test-odds-insert []
   (sql/insert! tu/*db-conn* :odds (tu/gen-fake-odds-info)))
 
-(defn query-test-db []
-  (sql/query tu/*db-conn* ["select count(*) from odds"]))
+(defn query-test-db [query]
+  (sql/query tu/*db-conn* [query]))
+
+(defn gen-odds-info
+  ([] (gen-odds-info 42))
+  ([seed]
+   (-> (mg/generate sbspec/odds-info {:seed seed})
+       (update :timestamp t/instant->sql-timestamp))))
 
 (deftest insert-fake-row
   (tu/with-test-db
     (test-odds-insert)
-    (is (= [{:count 1}] (query-test-db)))))
+    (is (= [{:count 1}] (query-test-db "select count(*) from odds")))))
+
+(deftest insert-new-matchup
+  (tu/with-test-db
+    (let [odds (gen-odds-info)]
+      (store/store-matchup odds)
+      (is (= [{:count 1}] (query-test-db "select count(*) from matchup"))))))
+
+
