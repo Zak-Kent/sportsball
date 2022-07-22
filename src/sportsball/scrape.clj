8ef4ff7b6c8cb2dfd2d23c3d8b8e67783a8e187e
Zak-Kent <zak.kent@gmail.com>
2022-07-22 17:18:57 -0400

    Update scraper to capture game score
    This commit adjusts the scraper so the game score is captured and added
    to the result map.

--- src/sportsball/scrape.clj
+++ src/sportsball/scrape.clj
@@ -36,8 +36,17 @@
                              #(str/starts-with? % prefix)
                              (parse-classes x))))))
 
+(defn str->int [s]
+  (try
+    (Integer/parseInt s)
+    (catch java.lang.NumberFormatException e
+      ;; TODO: add logging
+      (prn e)
+      nil)))
+
 
 ;; sportsbookreview specific scraping funcs
+
 (defn get-teams [teams]
   (let [extract (fn [t] (-> t :content first :content first))]
     (->> teams
@@ -66,6 +75,19 @@
     {:home-odds (extract home-odds)
      :away-odds (extract away-odds)}))
 
+(defn get-game-scores [page]
+  (let [scores (hs/select (hs/descendant
+                           (class-prefix-selector "finalScore"))
+                          page)
+        build-game-score (fn [[a h]]
+                           {:away-score a
+                            :home-score h})
+        get-score (fn [s]
+                    (->> (:content s)
+                         (map #(-> % :content first str->int))
+                         build-game-score))]
+    (map get-score scores)))
+
 (defn sportsbookreview->odds-infos
   "Takes html, uses hickory to parse the html into Clojure data, then extracts
    the odds for the games found on a sportsbookreview live MLB odds page."
@@ -93,23 +115,34 @@
                                (class-prefix-selector "oddsNumber")) %))
                        (map #(partition 2 %))
                        (map #(map get-book-odds %))
-                       (map #(zipmap (map keyword books) %)))]
-    (map (fn [[teams odds]]
-           {:books odds
-            :teams teams})
-         (map vector teams book-odds))))
+                       (map #(zipmap (map keyword books) %)))
+        scores (get-game-scores page)
+        ;; add nils for games which haven't yet started
+        scores-w-nils (concat
+                       scores
+                       (repeat (- (count teams) (count scores)) nil))]
+
+    (assert (= (count teams) (count book-odds))
+            "Number of teams and book odds didn't match!")
+
+    (map (fn [[t o s]]
+           {:books o
+            :teams t
+            :game-score s})
+         (map vector teams book-odds scores-w-nils))))
 
 (comment
   (let [url "https://www.sportsbookreview.com/betting-odds/mlb-baseball/money-line/"
         game-url "https://www.sportsbookreview.com/betting-odds/mlb-baseball/4677364/odds/"
-        extract-func (fn [html] (spit "game-scrape.html" html))]
+        extract-func (fn [html] (spit "live-game-scrape.html" html))]
 
-    (html->data game-url extract-func))
+    (html->data url extract-func))
 
   )
 
 (comment
-  (let [f (slurp "game-scrape.html")]
+  (let [f (slurp "live-game-scrape.html")]
+
     (clojure.pprint/pprint (sportsbookreview->odds-infos f)))
 
   )
