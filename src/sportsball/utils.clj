9b1c4338a1a2898f17f5856084aafda9e0f2a2f9
Zak-Kent <zak.kent@gmail.com>
2022-08-10 17:31:06 -0400

    Add helper to convert csv rows to maps
    This commit adds a helper to convert the rows of a csv into clojure
    maps.

--- src/sportsball/utils.clj
+++ src/sportsball/utils.clj
@@ -6,3 +6,9 @@
   [& args]
   (with-out-str
     (apply pprint/pprint args)))
+
+(defn csv->row-maps [header rows]
+  (let [header-keys (map keyword header)
+        convert-row (fn [row]
+                      (zipmap header-keys row))]
+    (map convert-row rows)))
